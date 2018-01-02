b0682a6d13eb711c8dbb23ceba585573a4c711f0
Minervias <kylewsipe@gmail.com>
2018-01-02 16:53:10 -0600

    Update innovacoin.py

--- p2pool/networks/innovacoin.py
+++ p2pool/networks/innovacoin.py
@@ -6,20 +6,20 @@ from p2pool.bitcoin import networks
 # REAL_CHAIN_LENGTH must be changed in sync with all other clients
 # changes can be done by changing one, then the other
 
-PARENT = networks.nets['phoenixcoin']
-SHARE_PERIOD = 30  # seconds
+PARENT = networks.nets['innovacoin']
+SHARE_PERIOD = 24  # seconds
 CHAIN_LENGTH = 60*60//10  # shares
 REAL_CHAIN_LENGTH = 60*60//10  # shares
 TARGET_LOOKBEHIND = 200  # shares
-SPREAD = 20  # blocks
-IDENTIFIER = '50686F656E69784C'.decode('hex')  # PhoenixL
-PREFIX = '5058432D4C697665'.decode('hex')  # PXC-Live
-P2P_PORT = 10555
+SPREAD = 15  # blocks
+IDENTIFIER = '19FFDB6CEB80E13B'.decode('hex')  # Innova
+PREFIX = '7B2861EB7F2BD772'.decode('hex')  # INN-Live
+P2P_PORT = 14521
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**20 - 1
 PERSIST = True
-WORKER_PORT = 10554
-BOOTSTRAP_ADDRS = 'atlas.phoenixcoin.org menoetius.phoenixcoin.org prometheus.phoenixcoin.org'.split(' ')
-ANNOUNCE_CHANNEL = '#p2pool-pxc'
+WORKER_PORT = 14520
+BOOTSTRAP_ADDRS = 'inn.swiftpool.org'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-inn'
 VERSION_CHECK = lambda v: True
-VERSION_WARNING = lambda v: 'Upgrade Phoenixcoin to >= 0.6.6.0!' if v < 60600 else None
+VERSION_WARNING = lambda v: 'Upgrade Innovacoin to >= 0.6.6.0!' if v < 60600 else None
